efe746fed5e1b38a03b212e20bbaccb26c2f5ab1
Jonathan De Ocampo <jonathandeocampo06@gmail.com>
2025-02-17 17:00:42 +0800

    add module exports

--- src/server.js
+++ src/server.js
@@ -35,3 +35,12 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
