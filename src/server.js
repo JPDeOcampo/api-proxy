d60042f2286eb794a7c348866fee67a1608f86ff
Jonathan De Ocampo <jonathandeocampo06@gmail.com>
2025-04-22 17:44:44 +0800

    catch error on app listen

--- src/server.js
+++ src/server.js
@@ -50,6 +50,8 @@ if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+  }).on('error', (err) => {
+    console.error('Server error:', err);
   });
 }
 
